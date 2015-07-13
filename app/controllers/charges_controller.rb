a278dfdc1cfe375aa6f9f7abc589e6eb25eb7016
John Clune <jmclune@gmail.com>
2015-07-13 12:06:00 -0400

    Implemented subcription based product GROHACK2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,26 @@
 class ChargesController < ApplicationController
   def create
   # Amount in cents
-  product = Product.find_by_sku("GROHACK1")
+  product = Product.find_by_sku("GROHACK2")
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
-    :card  => params[:stripeToken]
+    :card  => params[:stripeToken],
+    :plan  => "GROHACK2"
   )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => product.price_in_cents,
-    :description => product.full_description,
-    :currency    => 'usd'
-  )
+  # charge = Stripe::Charge.create(
+  #   :customer    => customer.id,
+  #   :amount      => product.price_in_cents,
+  #   :description => product.full_description,
+  #   :currency    => 'usd'
+  # )
+
  purchase = Purchase.create(
             email: params[:stripeEmail],
             card: params[:stripeToken],
             amount: @amount,
-            description: charge.description,
-            currency: charge.currency,
+            description: product.full_description,
+            currency: 'usd',
             customer_id: customer.id,
             product_id: product.id,
             uuid: SecureRandom.uuid)
